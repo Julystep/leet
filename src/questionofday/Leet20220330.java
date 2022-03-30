8894dfb92512e03dda80c587d733072cfddb8ac2
liml <967904176@qq.com>
2022-03-30 11:19:57 +0800

    提交

---
+++ src/questionofday/Leet20220330.java
@@ -0,0 +1,41 @@
+package questionofday;
+
+
+import java.util.*;
+
+
+
+public class Leet20220330 {
+
+    public List<Integer> busiestServers(int k, int[] arrival, int[] load) {
+        TreeSet<Integer> available = new TreeSet<>();
+        for (int i = 0; i < k; i++) {
+            available.add(i);
+        }
+        PriorityQueue<int[]> busy = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        int[] requests = new int[k];
+        for (int i = 0; i < arrival.length; i++) {
+            while (!busy.isEmpty() && busy.peek()[0] <= arrival[i]) {
+                available.add(busy.poll()[1]);
+            }
+            if (available.isEmpty()) {
+                continue;
+            }
+            Integer p = available.ceiling(i % k);
+            if (p == null) {
+                p = available.first();
+            }
+            requests[p]++;
+            busy.offer(new int[]{arrival[i] + load[i], p});
+            available.remove(p);
+        }
+        int maxRequest = Arrays.stream(requests).max().getAsInt();
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            if (requests[i] == maxRequest) {
+                ret.add(i);
+            }
+        }
+        return ret;
+    }
+}

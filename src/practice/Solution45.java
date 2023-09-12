ea89d72e79d58fe7ae42f782a386922895ec52f9
boomshair <957904176@qq.com>
2023-09-12 10:59:31 +0800

    55题

--- src/practice/Solution45.java
+++ src/practice/Solution45.java
@@ -6,6 +6,9 @@ package practice;
 
 public class Solution45 {
 
+    /**
+     * 倒着查的贪心算法虽然简单，但是时间复杂度还是有点高
+     */
     public int jump(int[] nums) {
         int position = nums.length - 1;
         int step = 0;
@@ -16,6 +19,21 @@ public class Solution45 {
                     step++;
                     break;
                 }
+            }
+        }
+        return step;
+    }
+
+    public int jump1(int[] nums) {
+        int len = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int step = 0;
+        for (int i = 0; i < len; i++) {
+            maxPosition = Math.max(nums[i] + i, maxPosition);
+            if (i == end) {
+                end = maxPosition;
+                step++;
             }
         }
         return step;
